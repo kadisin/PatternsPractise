4f0a31f249cec492fa022339ab083f30d736bf18
Tomasz Logisz <tomasz.logisz@capgemini.com>
2024-05-14 13:54:18 +0200

    Add singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * 
+ * Singleton is pattern use to get only one and same instance in global 
+ * Example is processing database...
+ * 
+ */
+
+using SingletonPattern.Singleton;
+
+var singleton1 = DatabaseSingleton.GetInstance();
+var singleton2 = DatabaseSingleton.GetInstance();
+
+//should be equal reference
+if(singleton1 == singleton2)
+{
+    Console.WriteLine("We got only one instance");
+}
+else
+{
+    Console.WriteLine("Oooops, something went wrong!");
+}
+
+singleton1.QueryDatabase();
+
+//We got only one instance
---
+++ SingletonPattern/Singleton/DatabaseSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.Singleton
+{
+    public class DatabaseSingleton
+    {
+        private DatabaseSingleton()
+        { }
+
+        private static DatabaseSingleton _instance;
+
+        public static DatabaseSingleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new DatabaseSingleton();
+            }
+            return _instance;
+        }
+
+        public void QueryDatabase()
+        {
+            //query to db...
+        }
+
+    }
+}
